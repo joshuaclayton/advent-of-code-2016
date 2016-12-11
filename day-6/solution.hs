2702f4ddd2f53fc1940466d0f38b51e1a9cca7af
Joshua Clayton <joshua.clayton@gmail.com>
2016-12-11 17:49:01 -0500

    Day 6, Solution 1
    Not much to see here. Data.Text.transpose did most of the heavy lifting.

---
+++ day-6/solution.hs
@@ -0,0 +1,33 @@
+#!/usr/bin/env stack
+-- stack --install-ghc runghc --package text
+
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Control.Arrow ((&&&))
+import qualified Data.List as L
+import           Data.Text (Text)
+import qualified Data.Text as T
+
+newtype Signal = Signal { signal :: [(Char, Int)] }
+
+main :: IO ()
+main = either (printError . show) run . parseInput . T.pack =<< getContents
+
+printError :: String -> IO ()
+printError e = do
+    putStrLn "There was a problem parsing the input:\n"
+    putStrLn e
+
+run :: [Signal] -> IO ()
+run = print . map mostFrequentFromSignal
+
+mostFrequentFromSignal :: Signal -> Char
+mostFrequentFromSignal = fst . last . L.sortOn snd . signal
+
+parseInput :: Text -> Either Text [Signal]
+parseInput = Right . map toSignal . T.transpose . T.lines
+  where
+    toSignal = Signal . frequency . T.unpack
+
+frequency :: Ord a => [a] -> [(a, Int)]
+frequency = map (head &&& length) . L.group . L.sort
