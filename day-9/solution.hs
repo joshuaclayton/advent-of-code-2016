3d4a4759d308058fd1fbd4869a1f846e1495c3e7
Joshua Clayton <joshua.clayton@gmail.com>
2016-12-15 11:09:53 -0500

    Day 9, Solution 1
    Megaparsec made this fairly easy with `count`.

---
+++ day-9/solution.hs
@@ -0,0 +1,64 @@
+#!/usr/bin/env stack
+-- stack --install-ghc runghc --package text --package megaparsec
+
+import           Control.Monad (void)
+import           Data.Text (Text)
+import qualified Data.Text as T
+import           Text.Megaparsec
+import qualified Text.Megaparsec.Lexer as L
+import           Text.Megaparsec.Text
+
+newtype Times = Times Int deriving Show
+
+data Chunk
+    = Pure Text
+    | Repeated Text Times
+    deriving Show
+
+main :: IO ()
+main = either (printError . show) run . parseInput . T.pack =<< getContents
+
+printError :: String -> IO ()
+printError e = do
+    putStrLn "There was a problem parsing the input:\n"
+    putStrLn e
+
+run :: [Chunk] -> IO ()
+run = print . T.length . T.concat . map chunkToText
+
+chunkToText :: Chunk -> Text
+chunkToText (Pure t) = t
+chunkToText (Repeated t (Times i)) = T.concat $ replicate i t
+
+parseInput :: Text -> Either ParseError [Chunk]
+parseInput = runParser (encryptedTextParser <* eof) "" . T.strip
+
+encryptedTextParser :: Parser [Chunk]
+encryptedTextParser = many $ repeatedChunk <|> pureChunk
+
+pureChunk :: Parser Chunk
+pureChunk = Pure . T.pack <$> some (noneOf "(")
+
+repeatedChunk :: Parser Chunk
+repeatedChunk = do
+    (length', times) <- parens lengthAndRepeat
+    parsedText <- T.pack <$> count length' anyChar
+    return $ Repeated parsedText times
+
+lengthAndRepeat :: Parser (Int, Times)
+lengthAndRepeat = do
+    length' <- fromInteger <$> L.integer
+    string "x"
+    times <- Times . fromInteger <$> L.integer
+    return (length', times)
+
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+
+symbol :: String -> Parser String
+symbol = L.symbol sc
+
+sc :: Parser () -- space consumer
+sc = L.space (void $ char ' ') lineComment blockComment
+  where lineComment  = L.skipLineComment "//"
+        blockComment = L.skipBlockComment "/*" "*/"
